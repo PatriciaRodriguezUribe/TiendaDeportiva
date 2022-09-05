33c62fb36efbe17ff1a5769468db8d3737251d69
PatriciaRodriguezUribe <110411278+PatriciaRodriguezUribe@users.noreply.github.com>
2022-09-05 18:25:56 -0500

    Add files via upload

--- TiendaDeportiva/src/main/java/Conexion/DBConnection.java
+++ TiendaDeportiva/src/main/java/Conexion/DBConnection.java
@@ -4,10 +4,38 @@
  */
 package Conexion;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 /**
  *
  * @author Patricia Rodriguez U
  */
 public class DBConnection {
+    Connection connection;
+    static String bd = "movies_rental";
+    static String port = "3306";
+    static String login = "root";
+    static String password = "admin";
+
+    public DBConnection() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:" + DBConnection.port + "/" + DBConnection.bd;
+            connection = DriverManager.getConnection(url, DBConnection.login, this.password);
+            System.out.println("Te has conectado a la Base de Datos: "+bd);
+        } catch (Exception ex) {
+            System.out.println("Error");
+        }
+    }
+
     
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void desconectar() {
+        connection = null;
+    }
 }
+
--- TiendaDeportiva/src/main/java/test/OperacionesDB.java
+++ TiendaDeportiva/src/main/java/test/OperacionesDB.java
@@ -1,27 +1,33 @@
 
 package test;
 
+import Conexion.DBConnection;
+
 
 public class OperacionesDB {
     
     public static void main(String[] args) {
-        
+        testBD();
     }
    
     public static void actualizarropa_femenina(int id_prenda, String tipo_prenda, String marca, String talla, String color, String estilo, int precio, boolean disponible){ 
        DBConnection con = new DBConnection(); 
-       String sql = "UPDATE ropa_femenina SET genero = '" + genero + "' WHERE id = " + id;//Variable que permite almacenar
-        try {//para que se conecte con sql usamos un try
-        Statement st = con.getConnection().createStatement();
-        st.executeUpdate(sql);
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
-        finally{
-            con.desconectar();
-        }
+//       String sql = "UPDATE ropa_femenina SET genero = '" + genero + "' WHERE id = " + id;//Variable que permite almacenar
+//        try {//para que se conecte con sql usamos un try
+//        Statement st = con.getConnection().createStatement();
+//        st.executeUpdate(sql);
+//        } catch (Exception ex) {
+//            System.out.println(ex.getMessage());
+//        }
+//        finally{
+//            con.desconectar();
+//        }
                
                
                
-    }   
+    } 
+   public static void testBD(){
+       DBConnection con = new DBConnection(); 
+   }
 }
+
